67d9bfc65fd179f91e357aa93b1cc2f5bc1fe392
hepan <hep@hmgsoft.com>
2019-06-19 16:46:30 +0800

    [springboot] [stomp] 服务端增加心跳

--- springboot/stomp-demo/src/main/java/io/github/hepan/springboot/stomp/demo/config/WebsocketConfig.java
+++ springboot/stomp-demo/src/main/java/io/github/hepan/springboot/stomp/demo/config/WebsocketConfig.java
@@ -4,6 +4,7 @@ import io.github.hepan.springboot.stomp.demo.websocket.WebSocketHandlerIntercept
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -16,6 +17,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final static long HEART_BEAT=5000;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
@@ -25,8 +29,16 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // 设置接收客户端消息 的 路径前缀（不设置可以）
         registry.setApplicationDestinationPrefixes("/app");
+
+        ThreadPoolTaskScheduler te = new ThreadPoolTaskScheduler();
+        te.setPoolSize(1);
+        te.setThreadNamePrefix("wss-heartbeat-thread-");
+        te.initialize();
+
+
+
         // 设置接收客户端订阅 的 路径前缀（必须不设置，可以为空串）
-        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/topic").setHeartbeatValue(new long[]{HEART_BEAT,HEART_BEAT}).setTaskScheduler(te);
     }
 
 
