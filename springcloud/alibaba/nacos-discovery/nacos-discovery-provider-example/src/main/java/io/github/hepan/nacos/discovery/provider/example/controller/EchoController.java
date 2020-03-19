76b12c02b2c39dcfee22a35351d4c0624d5905e0
hepan <hep@hmgsoft.com>
2020-03-19 09:25:13 +0800

    alibaba cloud nacos config

--- springcloud/alibaba/nacos-discovery/nacos-discovery-provider-example/src/main/java/io/github/hepan/nacos/discovery/provider/example/controller/EchoController.java
+++ springcloud/alibaba/nacos-discovery/nacos-discovery-provider-example/src/main/java/io/github/hepan/nacos/discovery/provider/example/controller/EchoController.java
@@ -1,5 +1,7 @@
 package io.github.hepan.nacos.discovery.provider.example.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @createTime 2020/3/18 18:49
  **/
 @RestController
+@RefreshScope
 public class EchoController {
+
+    @Value("${demo.username}")
+
+    private String username;
+
     @GetMapping("/echo/{string}")
     public String echo(@PathVariable String string) {
         return "hello 1->" + string;
+    }
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello 1->" + username;
     }
 }
